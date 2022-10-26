3d88cc99c878406aedc36ac3015350b2118cc067
Eternity <eternitysoft89@hotmail.de>
2022-10-26 05:43:26 +0200

    -added hardcoded restriction message to not allowed teleportation

--- src/main/java/boeseset/gamephases/mixin/player/ServerPlayerEntityMixin.java
+++ src/main/java/boeseset/gamephases/mixin/player/ServerPlayerEntityMixin.java
@@ -1,10 +1,13 @@
 package boeseset.gamephases.mixin.player;
 
 import boeseset.gamephases.kube.GamePhasesEventJS;
+import dev.architectury.event.events.common.TickEvent;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,7 +22,9 @@ public class ServerPlayerEntityMixin {
             cancellable = true)
     private void beforeTeleport(ServerWorld destination, CallbackInfoReturnable<Entity> cir) {
         boolean allowed = GamePhasesEventJS.getPhases().values().stream().filter(phase -> phase.restricts(destination)).allMatch(phase -> phase.hasUnlocked((PlayerEntity) (Object) this));
+        PlayerEntity player = (PlayerEntity) (Object) this;
         if(!allowed) {
+            player.sendSystemMessage(new LiteralText("§4Your are not allowed to teleport to the destination.\nPhase not unlocked.§f."), Util.NIL_UUID);
             cir.cancel();
         }
     }
